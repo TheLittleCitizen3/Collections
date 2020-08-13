cc23a41673ff77471ab80ecc2a4ceb1127c54db0
thelittlecitizen3 <thelittlecitizen3@gmail.com>
2020-08-13 19:55:06 +0300

    added DictionaryOperations and StackOperations

--- Program.cs
+++ Program.cs
@@ -8,8 +8,7 @@ namespace Collection
     {
         static void Main(string[] args)
         {
-            int a = 0;
-            Console.WriteLine(a%2);
+
         }
     }
 
@@ -18,7 +17,11 @@ namespace Collection
         static void ListOperations(ref List<string> list)
         {
             ///validation check
-            if (list == null || list.Count == 0)
+            if (list == null)
+            {
+                return;
+            }
+            if (list.Count == 0)
             {
                 Console.WriteLine("The list is empty");
                 return;
@@ -28,13 +31,76 @@ namespace Collection
             int middleIndex = (listIndex % 2 == 0) ?  listIndex /2 :  listIndex / 2 + 1;
             list.RemoveAt(middleIndex);
 
+            ///if contains aram and index is odd it reverts list order
             if (list.Contains("mamas"))
             {
                 list.Add("6");
             }
-
+            if (list.Contains("aram"))
+            {
+                if (list.IndexOf("aram") %2 != 0)
+                {
+                    list.Reverse();
+                }
+            }
             
+            /// adds str[2,3,4] if list has 3 or more uniq items
+            if (list.Count >= 3)
+            {
+                List<string> uniqItemsList = new List<string>();
+                foreach (var item in list)
+                {
+                    if (!uniqItemsList.Contains(item))
+                    {
+                        uniqItemsList.Add(item);
+                    }
+                }
+                if (uniqItemsList.Count >= 3)
+                {
+                    List<string> str = new List<string>(new string[] { "2", "3", "4" });
+                    list.InsertRange(2, str);
+                }
+            }
+                        
+        }
+        
+        static void DictionaryOperations(ref IDictionary<string,int> dict)
+        {
+            if(dict == null)
+            {
+                return;
+            }
+            if (dict.Count == 0)
+            {
+                return;
+            }
+            int scubaValue = default;
+            if (dict.TryGetValue("scuba", out scubaValue))
+            {
+                if(scubaValue == 6)
+                {
+                    dict.Add("Empire", 6);
+                }
+            }
+        }
 
+        static void StackOperations(ref Stack<DateTime> dateStack)
+        {
+            if (dateStack == null)
+            {
+                return;
+            }
+            if (dateStack.Count == 0)
+            {
+                return;
+            }
+            DateTime todaysSate = DateTime.Today;
+            int compare = DateTime.Compare(dateStack.Peek(), todaysSate);
+            if (compare < 0)
+            {
+                dateStack.Pop();
+                dateStack.Push(todaysSate);
+            }
         }
     }
 }
