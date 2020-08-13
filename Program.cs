ae55210dea9b84dde37e2568c18d187b88d812f0
thelittlecitizen3 <thelittlecitizen3@gmail.com>
2020-08-13 19:13:09 +0300

    added ListOperation function

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Collection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            Console.WriteLine(a%2);
+        }
+    }
+
+    static class CollectionOperations
+    {
+        static void ListOperations(ref List<string> list)
+        {
+            ///validation check
+            if (list == null || list.Count == 0)
+            {
+                Console.WriteLine("The list is empty");
+                return;
+            }
+            /// getting midle index to remove from list
+            int listIndex = list.Count();
+            int middleIndex = (listIndex % 2 == 0) ?  listIndex /2 :  listIndex / 2 + 1;
+            list.RemoveAt(middleIndex);
+
+            if (list.Contains("mamas"))
+            {
+                list.Add("6");
+            }
+
+            
+
+        }
+    }
+}
